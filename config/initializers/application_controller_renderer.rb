c03fc36b52e7bbe6121e15b097ac5b94a5c7b83a
sukepio <naoto927sukena@gmail.com>
2020-12-22 14:05:54 +0000

    [Finish] 2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '42c75917579c486398cde9b805ccb6f7b81f15f04f167238ad069ba516825fb50aca34c745a2435ab5b9f5f560f13ea356342888da5025a40f16c8f9ed089e9a'
